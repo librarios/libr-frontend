2148849ac04d4e392aab287b966d5e895b773c24
Lechuck Roh <member@lechuck2011.cafe24.com>
2020-03-18 22:18:20 +0900

    add searched book

--- src/BookSearch.svelte
+++ src/BookSearch.svelte
@@ -1,10 +1,11 @@
 <script>
   import axios from 'axios';
-  import {Button, Card, TextField} from 'smelte';
+  import {Button, Card, Snackbar, TextField} from 'smelte';
 
   let query = 'go';
   let result = [];
   let searching = false;
+  let showAddedSnackbar = false;
 
   async function onSearch() {
     const url = `${process.env.API_BASE_URL}/book/search?title=${query}`;
@@ -16,6 +17,19 @@
       console.error(e);
     } finally {
       searching = false;
+    }
+  }
+
+  async function onAdd(book) {
+    const url = `${process.env.API_BASE_URL}/books`;
+    try {
+      await axios.post(url, {
+        isbn: book.isbn13,
+        detail: book,
+      });
+      showAddedSnackbar = true;
+    } catch (e) {
+      console.error(e);
     }
   }
 </script>
@@ -31,18 +45,26 @@
   </div>
 </div>
 
-{#each result as book}
-  <Card.Card>
-    <div slot="title">
-      <Card.Title title="{book.title}" subheader="{book.authors.join(', ')}" />
-    </div>
-    <div slot="text" class="p-5 pb-0 pt-3 text-gray-700 body-2">
-      {book.contents}
-    </div>
-    <div slot="actions">
-      <div class="p-2">
-        <Button text>ADD</Button>
+<Snackbar top bind:value={showAddedSnackbar} timeout={2000}>
+  <div>Book added</div>
+</Snackbar>
+
+<div class="flex flex-row flex-wrap">
+  {#each result as book}
+    <Card.Card class="flex-auto m-2 w-64 card">
+      <div slot="title">
+        <Card.Title
+          title="{book.title}"
+          subheader="{book.authors.join(', ')}" />
       </div>
-    </div>
-  </Card.Card>
-{/each}
+      <div slot="text" class="p-5 pb-0 pt-3 text-gray-700 body-2">
+        {book.contents}
+      </div>
+      <div slot="actions">
+        <div class="p-2">
+          <Button text on:click="{onAdd(book)}">ADD</Button>
+        </div>
+      </div>
+    </Card.Card>
+  {/each}
+</div>
